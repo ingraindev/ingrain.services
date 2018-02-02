5e04b988d78c854df327e77eff7e69ea7dd87e84
murali <murali@LENOVO-PC>
2018-02-02 10:57:14 -0600

    Added Contact Controller

---
+++ Ingrain.Services/Ingrain.Services/Controllers/ContactController.cs
@@ -0,0 +1,33 @@
+﻿using Ingrain.Services.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ingrain.Services.Controllers
+{
+    [Produces("application/json")]
+  
+    public class ContactController :Controller
+    {
+        [Route("api/Contact")]
+        public IActionResult Get()
+        {
+            var resp = new Response();
+            // Pending integration with Services , to store data in database
+            return Ok(resp);
+        }
+
+        [HttpPost]
+        [Route("api/Contact")]
+        public IActionResult Post([FromBody] Employee emp)
+        {
+            var resp = new Response();
+            return Ok(resp);
+        }
+    }
+
+    public class Response
+    {
+        public int ReturnCd { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Ingrain.Services/Ingrain.Services/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace Ingrain.Services.Models
+{
+    public class Employee
+    {
+        public string ReferenceId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Address1 { get; set; }
+
+        public string Address2 { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+
+    }
+}
